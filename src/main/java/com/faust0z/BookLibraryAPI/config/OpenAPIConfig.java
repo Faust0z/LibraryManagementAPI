ade9b653e5b32d063731e3281f1b21a983c52e57
Faust0z <fausto.z77@hotmail.com>
2026-02-02 17:50:54 -0300

    Created a new config for the Swagger UI.

---
+++ src/main/java/com/faust0z/BookLibraryAPI/config/OpenAPIConfig.java
@@ -0,0 +1,37 @@
+package com.faust0z.BookLibraryAPI;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        Info info = new Info()
+                .title("Library Management API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage a library.")
+                .contact(new Contact().name("Faust0z").email("fausto.z77@hotmail.com"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
+    }
+}
